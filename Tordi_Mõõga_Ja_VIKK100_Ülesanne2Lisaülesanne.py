eb988db7832b18ab0077451c7ad0878236c7fabc
alex.zapotolok <alex.zapotolok@vikk.ee>
2025-03-13 11:25:31 +0200

    pygame

--- Tordi_Mõõga_Ja_VIKK100_Ülesanne2Lisaülesanne.py
+++ Tordi_Mõõga_Ja_VIKK100_Ülesanne2Lisaülesanne.py
@@ -15,16 +15,16 @@ chat = pygame.image.load("chat.png")
 logo = pygame.image.load("VIKK logo.png")
 sword = pygame.image.load("Mõõk.png")
 tort = pygame.image.load("ai-generated-cute-birthday-cake-in-cartoon-style-png.webp")
-tekst = pygame.image.load("inkpx-curved-text.png")
 
-tekst_pos = (309, 10)
+
+
 
 seller = pygame.transform.scale(seller, (248, 294))
 chat = pygame.transform.scale(chat, (230, 183))
 logo = pygame.transform.scale(logo, (265. , 45))
 sword = pygame.transform.scale(sword, (173, 143))
 tort = pygame.transform.scale(tort, (100, 93))
-tekst = pygame.transform.scale(tekst, (500, 300))
+
 
 # Fondi seaded
 font = pygame.font.Font(None, 26)
@@ -47,7 +47,6 @@ while running:
     screen.blit(text, (236, 140))
     screen.blit(tort, (420, 200))
     screen.blit(sword, (515, 100))
-    screen.blit(tekst, (120, 100))
 
 
     pygame.display.flip()
